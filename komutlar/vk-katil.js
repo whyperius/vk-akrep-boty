af5a72920b24dac8c49a637e8fb4d3b82068c977
Glitch (hello-express) <none>
2020-03-30 10:55:51 +0000

    🏈🐣 Checkpoint ./komutlar/denem.js:3756271/977 ./komutlar/vk-katil.js:3756271/851

--- komutlar/vk-katil.js
+++ komutlar/vk-katil.js
@@ -6,19 +6,16 @@ exports.run = (client, message, args) => {
 
   const roller = ["Vampir", "Köylü", "Savcı", "Hakim", "Doktor", "Medyum", "Şerif"]
   const rolata = roller[Math.floor(Math.random()*roller.length)];
-  message.author.send(`${rolata}`);
   
   const embed = new Discord.RichEmbed()
-     .setColor("#0080FF")
-    .setAuthor(client.user.username,client.user.displayAvatarURL)
+     .setColor('RANDOM')
     .setDescription(`
  <@${message.author.id}> ${rolata}
 `,true)
-    
-    .setTimestamp()
-    .setFooter(`Vampir Köylü Oyunu..`)
+        .setFooter(`Vampir Köylü Oyunu..`)
     let vkkimne = message.guild.channels.find(`name`, "vk-kim-ne");    
     vkkimne.send(embed)
+    message.author.send(embed)
 };
 
 exports.conf = {
