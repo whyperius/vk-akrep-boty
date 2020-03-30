1b0c4e14afde74e617b568f94ed2e02895a5c4d5
Glitch (hello-express) <none>
2020-03-30 09:21:52 +0000

    👟🍅 Checkpoint ./komutlar/kedi.js:3756271/1644 ./komutlar/vk-katil.js:3756271/3401

---
+++ komutlar/vk-katil.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['vk-katil'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'vk-katil',
+  description: 'Vampir Köylü Katılma.',
+  usage: 'vk-katil'
+};
