7811de355ac67cacafc0c473a54b776869237d40
Glitch (hello-express) <none>
2020-04-01 19:23:40 +0000

    🎺🌍 Checkpoint ./komutlar/korona.js:3756271/1101

---
+++ komutlar/korona.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'korona',
+  description: 'İstediğiniz şeyi bota yazdırır.',
+  usage: 'korona'
+};
