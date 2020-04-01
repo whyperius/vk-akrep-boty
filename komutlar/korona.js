155a149471eb8ad3abb911e76fc39137eb4302ae
Glitch (hello-express) <none>
2020-04-01 19:47:34 +0000

    🎊⏱ Checkpoint ./komutlar/korona.js:3756271/5159

--- komutlar/korona.js
+++ komutlar/korona.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-    const embed = new Discord.RichEmbed()
+    const korona = new Discord.RichEmbed()
       .setColor("#0080FF")
       .setAuthor("∼ Saudade Mudita ∼" , "https://cdn.discordapp.com/attachments/694990915617554482/694992053943402668/m_obBlVh.png")
       .setDescription(`⌘ Korona ⌘
@@ -11,13 +11,21 @@ Koronavirüs hastalığı **(COVID-19)** yeni bir virüsten kaynaklanan bulaşı
 Hastalık **kuru öksürük**, **ateş** ve daha ciddi vakalarda **nefes** almada zorluk gibi semptomlarla birlikte solunum yolu sorununa **(grip gibi)** yol açar.
 
 **Önlemi Nedir?**
-Ellerinizi sık sık yıkayarak, yüzünüze dokunmaktan kaçınarak ve kendisini iyi hissetmeyen kişilerle yakın temasta (1 metre) bulunmayarak kendinizi koruyabilirsiniz.
+Ellerinizi sık sık yıkayarak, yüzünüze dokunmaktan kaçınarak ve kendisini iyi hissetmeyen kişilerle yakın temasta **(1 metre)** bulunmayarak kendinizi koruyabilirsiniz.
 
+**Önemli Not:** Corona virüsü tüm dünyada yayılmaya başladı ve her ne kadar istemesek de ölümlere sebep olabiliyor. Bu sebeple hastanelere acil değilse gidilmesi durduruldu.
+**#EVDEKAL**
 
-`)
+Sorusu Olanlar İçin Sağlıkcı Arkadaşımızı Buraya Etiketliyorum. Soru Sormak İsteyenler Sağlıkçı Arkadaşımıza Özelden Yazabilirler.`)
       .setTimestamp()
       .setFooter(`${message.author.username} Tarafından İstendi`)
-       message.channel.send(embed)
+    
+    
+    const hesap = new Discord.RichEmbed()
+      .setColor("#0080FF")
+      .setDescription(`⌘ <@669598135999528971> ⌘`)
+       message.channel.send(korona)
+       message.channel.send(hesap)
 };
 
 exports.conf = {
@@ -29,6 +37,6 @@ exports.conf = {
 
 exports.help = {
   name: 'korona',
-  description: 'İstediğiniz şeyi bota yazdırır.',
+  description: 'Korona Hakkında Bilgi Verir.',
   usage: 'korona'
 };
