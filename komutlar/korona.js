5afb73da28465e1d7f5d14e4d94d63eff8ca5691
Glitch (hello-express) <none>
2020-04-01 19:35:31 +0000

    🚪😼 Checkpoint ./komutlar/korona.js:3756271/1493

--- komutlar/korona.js
+++ komutlar/korona.js
@@ -1,7 +1,23 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  
+    const embed = new Discord.RichEmbed()
+      .setColor("#0080FF")
+      .setAuthor("∼ Saudade Mudita ∼" , "https://cdn.discordapp.com/attachments/694990915617554482/694992053943402668/m_obBlVh.png")
+      .setDescription(`⌘ Korona ⌘
+
+**Korona Nedir?** 
+Koronavirüs hastalığı **(COVID-19)** yeni bir virüsten kaynaklanan bulaşıcı bir hastalıktır.
+Hastalık **kuru öksürük**, **ateş** ve daha ciddi vakalarda **nefes** almada zorluk gibi semptomlarla birlikte solunum yolu sorununa **(grip gibi)** yol açar.
+
+**Önlemi Nedir?**
+Ellerinizi sık sık yıkayarak, yüzünüze dokunmaktan kaçınarak ve kendisini iyi hissetmeyen kişilerle yakın temasta (1 metre) bulunmayarak kendinizi koruyabilirsiniz.
+
+
+`)
+      .setTimestamp()
+      .setFooter(`${message.author.username} Tarafından İstendi`)
+       message.channel.send(embed)
 };
 
 exports.conf = {
