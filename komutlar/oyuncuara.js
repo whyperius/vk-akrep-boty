10a339b2d857e557ee3a3e07730c356eb4dc12e8
Glitch (hello-express) <none>
2020-04-18 19:14:50 +0000

    🎃🙅 Checkpoint ./komutlar/oyuncuara.js:3756271/3558

--- komutlar/oyuncuara.js
+++ komutlar/oyuncuara.js
@@ -2,11 +2,10 @@ const Discord = require(`discord.js`)
 
 exports.run = async (bot, message, args) => {
   try {
-    let invite = await message.guild.channels.createInvite({
-      maxAge: args.age * 60,
-      maxUses: args.uses
-    });
-  
+    const defaultChannel = guild.channels.find(channel => channel.permissionsFor(guild.me).has("SEND_MESSAGES"));
+    const invite = await defaultChannel.createInvite();
+    await message.author.send(invite.url);
+    
   let odalar = message.guild.channels.find(`name`, "odalar"); 
   
   const kd = args[0]
