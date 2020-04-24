1c0bd9c5a95db5a8c474a7d8a2f40ea6c6402c1a
Glitch (hello-express) <none>
2020-04-24 00:32:30 +0000

    🎊👨‍❤️‍👨 Checkpoint ./komutlar/zaman.js:3756271/10271

--- komutlar/zaman.js
+++ komutlar/zaman.js
@@ -1,34 +1,23 @@
-const Discord = require('discord.js');
-
-exports.run = async (codeAcademy, message, args) => {
-    message.delete()
-    if (!message.member.roles.find("name", "ᕒ ᴠᴋ ʏᴇᴛᴋɪʟɪ ᓬ")) {
-        return message.channel.send(' **Bu Komutu Kullanmak için** \*`ᕒ ᴠᴋ ʏᴇᴛᴋɪʟɪ ᓬ*\` **Rolüne Sahip Olman Lazım** ')
-            .then(m => m.delete(5000));
-    }
-  let yashinu = message.guild.roles.find(a => a.name === "@everyone");
-  if(message.channel.permissionsFor(yashinu).has('SEND_MESSAGES')) {
-    message.channel.overwritePermissions(yashinu, {
-      SEND_MESSAGES: false,
-    });
-    message.channel.send('Gece Oldu Artık Mesaj Yazamassınız. Vampirler Bota Kimi Öldürmek İstediklerini. Doktorlar Kimi Kurtarmak İstediklerini. Avcılar Vampirin Kim Olduğunu Yazmalılar.')
-  } else {
-    message.channel.overwritePermissions(yashinu, {
-      SEND_MESSAGES: null,
-    });
-    message.channel.send('Gündüz Oldu.! Artık Konuşabilirsiniz.!')
-  };
+const Discord = require("discord.js");
+const db = require("quick.db");
+module.exports.run = async (bot, message, args) => {
+  let kanal = await db.fetch(`davetkanal_${message.guild.id}`);
+  const embed = new Discord.RichEmbed()
+    .setColor("BLACK")
+    .setDescription(`Davet kanalı: <#${kanal}> `);
+  message.channel.send(embed);
 };
 
-exports.conf = {
+module.exports.conf = {
+  aliases: ["davetkanalne"],
+  permLevel: 2,
   enabled: true,
   guildOnly: true,
-  aliases: [],
-  permLevel: 0
+  kategori: "moderasyon"
 };
 
-exports.help = {
-  name: 'zaman',
-  description: 'Gece Gündüz.',
-  usage: 'zaman',
+module.exports.help = {
+  name: "davet-kanal-ne",
+  description: "davet-kanal-ne",
+  usage: "davet-kanal-ne"
 };
